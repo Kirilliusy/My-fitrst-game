081ee84e23b01d67522f033d236979657dde26c7
Kirilliusy <jigaloffkir@gmail.com>
2023-09-14 19:27:24 +0300

    Project
    Forst project

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private string _name;
+
+    private void Start()
+    {
+        transform.name = _name;
+        GetComponent<SpriteRenderer>().color = Color.red;
+    }
+    private void Update()
+    {
+
+        transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition) + new Vector3(0, 0, 10);
+        
+    }
+}
